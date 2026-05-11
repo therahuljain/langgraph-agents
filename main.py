3b9cc2e7340af81b2d3823d812057e5bf787811d
Rahul Jain <rahuljain0890@icloud.com>
2026-05-11 23:24:41 +0200

    created a very basic graph

---
+++ main.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+from typing import Annotated, Literal
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain.chat_models import init_chat_model
+from pydantic import BaseModel, Field
+from typing_extensions import TypedDict
+
+load_dotenv()
+
+llm = init_chat_model("gpt-5-mini", model_provider="openai", temperature=0.7)
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+graph_builder = StateGraph(State)
+
+def chatbot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
+
+
+graph_builder.add_node("chatbot", chatbot)
+
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+
+graph = graph_builder.compile()
+
+user_input = input("Enter your message: ")
+state = graph.invoke({"messages": [{"role": "user", "content": user_input}]})
+
+
+print(state["messages"][-1].content)
